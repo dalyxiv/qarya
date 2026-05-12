00b36758acd5bcea7bc23d59acb6539118fa9f6a
Mostafa Eldaly <dalyxiv@gmail.com>
2026-05-12 22:14:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from '@tanstack/start/config'
 
 export default defineConfig({
   server: {
-    preset: 'github-pages'
+    preset: 'github-pages',
+    prerender: {
+      // Tells the builder to generate a static index.html for the home page
+      routes: ['/'], 
+      // Automatically finds and generates static pages for any other linked routes
+      crawlLinks: true 
+    }
   },
   vite: {
     base: '/qarya/'
